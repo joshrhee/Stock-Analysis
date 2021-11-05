16554466fa461f1e8c81f19bc492e015b2f3792a
Sang June (Josh) Rhee <srhee34@gatech.edu>
2021-11-05 19:20:52 -0400

    Add basic chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+// Need to do:
+//    - Search company's name
+//    - get range of that company's stock price
+
+import { useState, useEffect } from 'react';
 import './App.css';
 
+// https://github.com/apexcharts/react-apexcharts
+import Chart from 'react-apexcharts';
+
+// For Cors error
+// const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+// const stockUrl = `${proxyUrl}https://query1.finance.yahoo.com/v8/finance/chart/GME`
+// async function getStocks() {
+//     const response = await fetch(stockUrl);
+//     return response.json();
+// }
+
+
+
+const chart = {
+  options: {
+    chart: {
+      id: 'apexchart-example'
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+    }
+  },
+  series: [{
+    name: 'series-1',
+    data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+  }]
+}
+
+
 function App() {
+  const [price, setPrice] = useState("loading");
+  const [priceTime, setPriceTime] = useState(null);
+
+  useEffect(() => {
+
+    // let timeoutId;
+    // async function getLatestPrice() {
+    //   const data = await getStocks();
+    //   const gme = data.chart.result[0];
+    //   setPrice(gme.meta.regularMarketPrice.toFixed(2))
+    //   setPriceTime(new Date(gme.meta.regularMarketTime * 1000))
+    //   // calling getLatestPrice() self again in every 5 seconds.
+    //   timeoutId = setTimeout(getLatestPrice, 5000);
+    // }
+
+    // // calling getLatestPrice() every 5 seconds.
+    // timeoutId = setTimeout(getLatestPrice, 5000);
+
+    // return () => {
+    //   clearTimeout(timeoutId)
+    // }
+
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Member: Alex, David, Rile, Sang June</h2>
+      <div className="price">
+        {/* {price} */}
+        {/* <br/> */}
+        {/* {priceTime && priceTime.toLocaleTimeString()} */}
+      </div>
+
+      <Chart options={chart.options} series={chart.series} type="bar" width={window.innerWidth} height={320} />
     </div>
   );
 }
